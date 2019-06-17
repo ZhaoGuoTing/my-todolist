6cd07b63d28fd99013f8012de7c5acb04b7a6837
ZhaoGuoTing <zhaoguoting@aliyun.com>
2019-06-17 17:22:44 +0800

    创建store
    往reducer里传值

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+// 引入redux框架 调用 createStore 方法
+import reducer from './reducer';
+// 引入笔记本 reducer.js
+
+const store = createStore(reducer);
+// 定义 store 为 createStore 方法，创建了一个数据的公共存储仓库
+
+export default store
+// 把store 导出去
---
+++ src/store/reducer.js
@@ -0,0 +1,17 @@
+const defaultState = {
+  // data: []
+  inputValue: '请输入',
+  list: [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ],
+}
+
+export default (state = defaultState, action) => {
+  return state;
+}
+// state 是整个图书馆的数据
+// action 是借书人的那句话
