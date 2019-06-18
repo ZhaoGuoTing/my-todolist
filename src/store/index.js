83d4ee4f5259711a024bebd7e9e393e91c06ecfb
ZhaoGuoTing <zhaoguoting@aliyun.com>
2019-06-18 16:58:14 +0800

    变成原有的样子
    1

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,6 @@ import reducer from './reducer';
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 // 定义 store 为 createStore 方法，创建了一个数据的公共存储仓库
 
+
 export default store
 // 把store 导出去
