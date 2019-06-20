8a6d571946ca0883fdfa4f0491682f73eeab34a4
ZhaoGuoTing <zhaoguoting@aliyun.com>
2019-06-20 13:11:06 +0800

    第5章Redux入门
    P3、P4、P5、P6的内容

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,61 +3,67 @@ import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd';
 import store from './store';
 
-
 export class TodoList extends Component {
 
   constructor(props) {
     super(props)
 
-    this.state = store.getState();
+    this.state = store.getState(); // 获取 store 的所有数据内容
     this._StoreChange = this._StoreChange.bind(this);
-    store.subscribe(this._StoreChange)
-
+    store.subscribe(this._StoreChange) // store 的数据发生改变，将新值传给组件
     
     this._onChangeInput = this._onChangeInput.bind(this);
     this._onClickBtn = this._onClickBtn.bind(this);
   }
-  
-
-  render() {
-    return (
-      <div style={{marginTop: '10px',marginLeft: '10px'}}>
-        <h1>Hello World</h1>
-        <Input 
-          placeholder = "请输入"
-          value={this.state.inputValue} 
-          style={{width: '300px',marginRight: '10px'}}
-          onChange={this._onChangeInput}
-        />
-        <Button type="primary" onClick={this._onClickBtn}>提交</Button>
-        <List
-          style={{marginTop: '10px', width: '300px'}}
-          bordered
-          dataSource={this.state.list}
-          renderItem={item => <List.Item>{item}</List.Item>}
-        />
-      </div>
-    )
-  }
-  _StoreChange() {
-    this.setState(store.getState())
+  _StoreChange() { // 当数据改变是 执行该函数
+    this.setState(store.getState()) // 获取 新数据内容
   }
 
-
-  _onChangeInput(e) {
-    const action = {
-      type: "change_input_value",
-      value: e.target.value
+    render() {
+        return (
+        <div style={{marginTop: '10px',marginLeft: '10px'}}>
+            <h1>Hello World</h1>
+            <Input 
+                placeholder = "请输入"
+                value={this.state.inputValue} 
+                style={{width: '300px',marginRight: '10px'}}
+                onChange={this._onChangeInput}
+            />
+            <Button type="primary" onClick={this._onClickBtn}>提交</Button>
+            <List
+                style={{marginTop: '10px', width: '300px'}}
+                bordered
+                dataSource={this.state.list}
+                renderItem={(item, index) => 
+                    <List.Item onClick={this._onClickListItemDel.bind(this, index)}>{item}</List.Item>
+                }
+            />
+        </div>
+        )
     }
-    store.dispatch(action);
-  }
-  
-  _onClickBtn() {
-    const action = {
-      type: "add_todo_Item",
+
+    _onChangeInput(e) {
+        const action = {
+            type: "change_input_value",
+            value: e.target.value
+        }
+        store.dispatch(action); // 发送 action 给 store
     }
-    store.dispatch(action);
-  }
+  
+    _onClickBtn() {
+        const action = {
+        type: "add_todo_Item",
+        }
+        store.dispatch(action);
+    }
+
+    _onClickListItemDel(index) {
+        const action = {
+        type: "del_todo_Item",
+        index
+        }
+        store.dispatch(action);
+    }
 }
 
 export default TodoList
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,20 +7,27 @@ const defaultState = {
   ],
 }
 
-// reducer 可以接受 state， 但是绝不能修改state
-// state = defaultState,是图书馆的默认旧数据、
-// action 是借书人的那句话
+// Reducers 接收(state， action)
+// state = defaultState、是以前默认旧数据
+// action、当前要执行的操作
 export default (state = defaultState, action) => {
   if (action.type === 'change_input_value') {
     const newState = JSON.parse(JSON.stringify(state)); // 定义 newState = 默认旧数据
     newState.inputValue = action.value; // 默认旧数据の.inputValue值 = action传过来の.新值
-    return newState;  // 做一个 返回
+    return newState;  // 把新数据返回 stroe 替换旧数据
   }
   if (action.type === 'add_todo_Item') {
     const newState = JSON.parse(JSON.stringify(state));
-    newState.list.push(newState.inputValue);
-    newState.inputValue = '';
+    if (newState.inputValue !== '') {
+      newState.list.push(newState.inputValue);
+      newState.inputValue = '';
+    }
     return newState;
   }
+    if (action.type === 'del_todo_Item') {
+      const newState = JSON.parse(JSON.stringify(state)); 
+      newState.list.splice(action.index, 1);
+      return newState; 
+    }
   return state;
 }
